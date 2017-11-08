1bb8fa49ca9fe02efb451164d3f5fabe00ea7c15
Gia <gia@coa.vn>
2017-11-09 00:55:21 +0700

    update bittrex signer, remove redundant function

--- exchange/bittrex/signer.go
+++ exchange/bittrex/signer.go
@@ -1,10 +1,6 @@
 package bittrex
 
 type Signer interface {
-	GetLiquiKey() string
 	GetBittrexKey() string
-	GetBinanceKey() string
-	LiquiSign(msg string) string
 	BittrexSign(msg string) string
-	BinanceSign(msg string) string
 }
