3a00958c33c441f440e0ca2f7f93508037d881b2
Victor Tran <vu.tran54@gmail.com>
2017-11-15 10:56:27 +0700

    update

--- cmd/simulation.go
+++ cmd/simulation.go
@@ -15,7 +15,7 @@ import (
 )
 
 func GetConfigForSimulation() *Config {
-	settingPath := "/go/src/github.com/KyberNetwork/reserve-data/cmd/deployment_dev.json"
+	settingPath := "/go/src/github.com/KyberNetwork/reserve-data/cmd/shared/deployment_dev.json"
 	addressConfig, err := common.GetAddressConfigFromFile(settingPath)
 	if err != nil {
 		log.Fatalf("Config file %s is not found. Error: %s", settingPath, err)
