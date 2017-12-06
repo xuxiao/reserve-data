1af70f5d54894d82711c12f90b7edfce978a8cf2
Victor Tran <vu.tran54@gmail.com>
2017-12-06 20:21:55 +0700

    add name to withdraw api

--- exchange/binance/binance_endpoint.go
+++ exchange/binance/binance_endpoint.go
@@ -291,6 +291,7 @@ func (self *BinanceEndpoint) Withdraw(token common.Token, amount *big.Int, addre
 		map[string]string{
 			"asset":   token.ID,
 			"address": address.Hex(),
+			"name":    "reserve",
 			"amount":  strconv.FormatFloat(common.BigToFloat(amount, token.Decimal), 'f', -1, 64),
 		},
 		true,
