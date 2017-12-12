d874539f6f0d06922bb5a0320ef1e3e707e7cb27
Victor Tran <vu.tran54@gmail.com>
2017-12-12 14:18:46 +0700

    fix unintentional string manipulation

--- common/util.go
+++ common/util.go
@@ -4,7 +4,8 @@ const TRUNC_LENGTH int = 600
 
 func TruncStr(src []byte) []byte {
 	if len(src) > TRUNC_LENGTH {
-		return append(src[0:TRUNC_LENGTH], []byte("...")...)
+		result := string(src[0:TRUNC_LENGTH]) + "..."
+		return []byte(result)
 	} else {
 		return src
 	}
