806d915d5733e86d1b63405af3075d245ddb1349
NNagato <darkdragon236@gmail.com>
2018-01-17 01:24:52 +0700

    handle-timestam[

--- blockchain/blocktime.go
+++ blockchain/blocktime.go
@@ -2,9 +2,18 @@ package blockchain
 
 import (
 	"time"
+	"context"
+	"math/big"
 )
 
-func InterpretTimestamp(blockno uint64, txindex uint) uint64 {
-	// TODO: Using real timestamp
-	return uint64(time.Second)*15*blockno + uint64(txindex)
+func (self *Blockchain) InterpretTimestamp(blockno uint64, txindex uint) (uint64, error) {
+	context := context.Background()
+	block, err := self.client.HeaderByNumber(context, big.NewInt(int64(blockno)))
+	if err != nil {
+		return uint64(0), err
+	}
+	unixSecond := block.Time.Uint64()
+	unixNano := uint64(time.Unix(int64(unixSecond), 0).UnixNano())
+	result := unixNano + uint64(txindex)
+	return result, nil
 }
