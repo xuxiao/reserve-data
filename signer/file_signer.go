6a011e73446db57b02a06aea4d5845ea7933af15
admin <halink@HaLinks-MacBook.local>
2018-01-16 13:55:47 +0700

    refactor

--- signer/file_signer.go
+++ signer/file_signer.go
@@ -109,7 +109,6 @@ func (self FileSigner) BinanceSign(msg string) string {
 func (self FileSigner) HuobiSign(msg string) string {
 	mac := hmac.New(sha256.New, []byte(self.HuobiSecret))
 	mac.Write([]byte(msg))
-	// result := ethereum.Bytes2Hex(mac.Sum(nil))
 	result := base64.StdEncoding.EncodeToString(mac.Sum(nil))
 	return result
 }
