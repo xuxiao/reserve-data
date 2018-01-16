ed6fe8a2cc16fdc56dd6655700a755f26e8599b3
halink0803 <halink0803@gmail.com>
2018-01-16 19:49:34 +0700

    update binance get deposit address

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ FROM golang
 # Copy the local package files to the container's workspace.
 ADD . /go/src/github.com/KyberNetwork/reserve-data
 
-ENV KYBER_EXCHANGES bittrex
+ENV KYBER_EXCHANGES binance
 WORKDIR /go/src/github.com/KyberNetwork/reserve-data
 RUN go install -v github.com/KyberNetwork/reserve-data/cmd
 
--- exchange/binance.go
+++ exchange/binance.go
@@ -39,7 +39,7 @@ func (self *Binance) UpdateAllDepositAddresses(address string) {
 }
 
 func (self *Binance) UpdateDepositAddress(token common.Token, address string) {
-	liveAddress, _ := self.interf.GetDepositAddress(token.ID)
+	liveAddress, _ := self.interf.GetDepositAddress(strings.ToLower(token.ID))
 	if liveAddress.Address != "" {
 		self.addresses[token.ID] = ethereum.HexToAddress(liveAddress.Address)
 	} else {
