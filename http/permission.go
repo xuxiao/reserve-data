f41940fa9349b6b49550f2ddfcb31748551936d6
Victor Tran <vu.tran54@gmail.com>
2018-01-25 14:34:49 +0700

    add permission

---
+++ http/permission.go
@@ -0,0 +1,9 @@
+package http
+
+type Permission int
+
+const (
+	ReadOnlyPermission  Permission = iota // can only read data
+	RebalancePermission                   // can do everything except configure setting
+	ConfigurePermission                   // can read data and configure setting, cannot set rates, deposit, withdraw, trade, cancel activities
+)
