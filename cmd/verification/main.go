0033cd5a682c86cbd0c986b44a60129ffc08f177
halink0803 <halink0803@gmail.com>
2018-03-01 18:47:35 +0700

    update script

--- cmd/verification/main.go
+++ cmd/verification/main.go
@@ -1,45 +1,50 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/KyberNetwork/reserve-data/cmd/configuration"
 )
 
+var noAuthEnable bool
+var endpointOW string
+
+func validateArgs() {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+func printUsage() {
+	fmt.Println("Usage: ./verification ACTION FLAGS")
+	fmt.Println(" deposit -amount AMOUNT -token TOKEN -base_url BASE_URL, base_url default is http://localhost:8000/")
+	fmt.Println(" withdraw -amount AMOUNT -token TOKEN -base_url BASE_URL, base_url default is http://localhost:8000/")
+}
+
+func run(verify *Verification) {
+	switch os.Args[1] {
+	default:
+		printUsage()
+	}
+}
+
 func main() {
 	var config *configuration.Config
-	switch os.Getenv("KYBER_ENV") {
-	case "mainnet", "production":
-		log.Printf("Running in production mode")
-		config = configuration.GetConfigForMainnet()
-		break
-	case "staging":
-		log.Printf("Running in staging mode")
-		config = configuration.GetConfigForStaging()
-		break
-	case "simulation":
-		log.Printf("Running in simulation mode")
-		config = configuration.GetConfigForSimulation()
-		break
-	case "kovan":
-		log.Printf("Running in kovan mode")
-		config = configuration.GetConfigForKovan()
-		break
-	case "ropsten":
-		log.Printf("Running in ropsten mode")
-		config = configuration.GetConfigForRopsten()
-		break
-	case "dev":
-		log.Printf("Running in dev mode")
-		config = configuration.GetConfigForDev()
-	default:
-		log.Printf("Running in dev mode")
-		config = configuration.GetConfigForDev()
+	kyberENV := os.Getenv("KYBER_ENV")
+	if kyberENV == "" {
+		kyberENV = "dev"
 	}
+	config = configuration.GetConfig(kyberENV,
+		!noAuthEnable,
+		endpointOW)
 	if config.AuthEngine == nil {
 		Warning.Println("Current environment setting does not enable authentication. Please check again!!!")
 	}
 	verify := NewVerification(config.AuthEngine)
-	verify.RunVerification()
+	// verify.RunVerification()
+	validateArgs()
+
+	run(verify)
 }
